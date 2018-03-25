0ca64739fa286e4bb280c87cc6ae533b40c93533
lancaiwu <lancaiwu@gmail.com>
2018-03-25 23:16:35 +0800

    init

--- zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -3,11 +3,13 @@ package com.example.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy // 开启zuul的api网关服务
 @SpringCloudApplication
+@EnableHystrixDashboard
 public class ZuulApplication {
 
     public static void main(String[] args) {
