5c78caf9d0296b06341ec6a8863e649f3fc0dfe8
lancaiwu <lancaiwu@gmail.com>
2018-03-28 22:57:43 +0800

    init

--- ConfigServer/src/main/java/com/example/config/server/ServerApplication.java
+++ ConfigServer/src/main/java/com/example/config/server/ServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 配置中心，同时配置刷新配置消息总线入口
+ */
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
